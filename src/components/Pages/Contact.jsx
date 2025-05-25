07dc694e3a3a59414c05c89feefa066f7e3d5d55
Deepak babu <21mh1a4242@acoe.edu.in>
2025-05-26 02:25:10 +0530

    updated

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -17,90 +17,79 @@ const ContactComponent = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const myForm = event.target;
-    const formData = new FormData(myForm);
+    const form = event.target;
 
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
+      body: new URLSearchParams(new FormData(form)).toString(),
     })
       .then(() => {
-        return alert("Message sent successfully!");
+        alert("Message sent successfully!");
+        setFormData({ username: "", email: "", message: "" });
       })
       .catch((error) => alert(error));
-
-    // Reset form fields after submission
-    setFormData({
-      username: "",
-      email: "",
-      message: "",
-    });
-
-    document.querySelector("form").addEventListener("submit", handleSubmit);
   };
 
   return (
-    <>
-      <article className="contact active" data-page="contact">
-        <header>
-          <h2 className="h2 article-title">Contact</h2>
-        </header>
+    <article className="contact active" data-page="contact">
+      <header>
+        <h2 className="h2 article-title">Contact</h2>
+      </header>
 
-        <section className="contact-form">
-          <h3 className="h3 form-title">Contact Form</h3>
+      <section className="contact-form">
+        <h3 className="h3 form-title">Contact Form</h3>
 
-          <form
-            name="contact-form"
-            method="POST"
-            data-netlify-honeypot="bot-field"
-            onSubmit={handleSubmit}
-            className="form"
-            netlify
-          >
-            {/* Hidden fields for Netlify */}
-            <input type="hidden" name="contact-form" value="contact" />
-            <input type="hidden" name="bot-field" />
+        <form
+          name="contact-form"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+          className="form"
+        >
+          {/* Hidden fields for Netlify */}
+          <input type="hidden" name="form-name" value="contact-form" />
+          <input type="hidden" name="bot-field" />
 
-            <div className="input-wrapper">
-              <input
-                type="text"
-                name="username"
-                className="form-input"
-                placeholder="Full name"
-                value={formData.username}
-                onChange={handleChange}
-                required
-              />
-
-              <input
-                type="email"
-                name="email"
-                className="form-input"
-                placeholder="Email address"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <textarea
-              name="message"
+          <div className="input-wrapper">
+            <input
+              type="text"
+              name="username"
               className="form-input"
-              placeholder="Your Message"
-              value={formData.message}
+              placeholder="Full name"
+              value={formData.username}
               onChange={handleChange}
               required
-            ></textarea>
+            />
 
-            <button className="form-btn" type="submit">
-              <ion-icon name="paper-plane"></ion-icon>
-              <span>Send Message</span>
-            </button>
-          </form>
-        </section>
-      </article>
-    </>
+            <input
+              type="email"
+              name="email"
+              className="form-input"
+              placeholder="Email address"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <textarea
+            name="message"
+            className="form-input"
+            placeholder="Your Message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+
+          <button className="form-btn" type="submit">
+            <ion-icon name="paper-plane"></ion-icon>
+            <span>Send Message</span>
+          </button>
+        </form>
+      </section>
+    </article>
   );
 };
 
